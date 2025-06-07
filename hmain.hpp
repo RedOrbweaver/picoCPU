25473c88ad7a050586857f5b7f67331522815051
RedOrbweaver <oliwier.ciesielski@proton.me>
2025-06-07 17:29:48 +0200

    ball test

--- hmain.hpp
+++ hmain.hpp
@@ -14,13 +14,15 @@
 #include "hardware/clocks.h"
 #include "hardware/uart.h"
 
+#include "redpicolib/RedPicoLib.hpp"
+
 #include "../picoGPU/shared.hpp"
 
 namespace PIN
 {
-    uint8_t DAC_OUT[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-    uint8_t SPI_RX = 16;
-    uint8_t SPI_CS = 17;
-    uint8_t SPI_SCK = 18;
-    uint8_t SPI_TX = 19;
+    constexpr uint8_t DAC_OUT[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+    constexpr uint8_t I2C_SCL = 17;
+    constexpr uint8_t I2C_SDA = 16;
 }
+
+constexpr uint8_t I2C_ADDR = 0x11;
--- picoCPU.cpp
+++ picoCPU.cpp
@@ -1,8 +1,9 @@
 #include "hmain.hpp"
 
 
-void SPIWrite(SOURCE source, uint8_t address0, uint8_t address1, uint8_t len, uint8_t* data)
+void I2CWrite(SOURCE source, uint8_t address0, uint8_t address1, uint8_t len, uint8_t* data)
 {
+    
     uint8_t buffer[255+5];
     buffer[0] = 0xFF; // WRITE
     buffer[1] = (uint8_t)source;
@@ -10,7 +11,8 @@ void SPIWrite(SOURCE source, uint8_t address0, uint8_t address1, uint8_t len, ui
     buffer[3] = address1;
     buffer[4] = len;
     memcpy(buffer + 5, data, len);
-    spi_write_blocking(spi0, buffer, len + 5);
+    i2c_write_blocking(i2c0, I2C_ADDR, buffer, len + 5, false);
+    
 }
 
 int main()
@@ -22,24 +24,51 @@ int main()
     uart_set_baudrate(uart0, 115200);
     printf("clock set successfully\n");
 
-    spi_init(spi0, 1000 * 100);
-    gpio_set_function(PIN::SPI_RX, GPIO_FUNC_SPI);
-    gpio_set_function(PIN::SPI_SCK, GPIO_FUNC_SPI);
-    gpio_set_function(PIN::SPI_TX, GPIO_FUNC_SPI);
-    gpio_set_function(PIN::SPI_CS, GPIO_FUNC_SPI);
+    i2c_init(i2c0, 2 * 1000 * 1000);
+    gpio_set_function(PIN::I2C_SCL, GPIO_FUNC_I2C);
+    gpio_set_pulls(PIN::I2C_SCL, true, false);
+    gpio_set_function(PIN::I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_pulls(PIN::I2C_SDA, true, false);
 
-    uint8_t val;;
+
+    int lines_x = 408;
+    int lines_y = 304;
+
+    float sx = 10.0f;
+    float sy = 10.0f;
+    float px = 50.00f;
+    float py = 50.0f;
+    float r = 15;
+
+    Entity ball;
+    ball.visible = true;
+    ball.type = ENTITY_TYPE::SHAPE;
+    ball.layer = 0;
+    ball.size = vec2<uint16_t>{uint16_t(r), uint16_t(r)};
+    ball.data[0] = (uint8_t)SHAPE::CIRCLE;
+    ball.data[1] = 128;
+    ball.data[2] = 0;
+
     while(true)
     {
-        val++;
-        std::string text = "this is a test!";
-        SPIWrite(SOURCE::DEBUG_PRITNF, 1, 2, text.length(), (uint8_t*)text.c_str());
-        sleep_ms(1000);
-    }
+        uint64_t ttm = get_time_us();
+        float nx = px + sx;
+        float ny = py + sy;
+        if(nx < r || nx > float(lines_x)-2*r)
+        {
+            sx = -sx;
+        }
+        else
+            px = nx;
+        if(ny < 2*r || ny > float(lines_y)-r)
+        {
+            sy = -sy;
+        }
+        else
+            py = ny;
 
-    while (true) 
-    {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        ball.pos = vec2<uint16_t>{(uint16_t)px, (uint16_t)py};
+        ball.rotation += 1;
+        I2CWrite(SOURCE::ENTITY_BUFFER, 0, 0, sizeof(Entity), (uint8_t*)&ball);
     }
 }
